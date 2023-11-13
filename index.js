bd074da44129260ddeb82083ee2eb3639e57c545
AnthonyRay <rayanthony42@gmail.com>
2023-11-13 17:10:50 -0500

    3fp code

---
+++ index.js
@@ -0,0 +1,5 @@
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguageOptional("Aki", "JavaScript"));
+console.log(introductionWithLanguageOptional("Aki"));
